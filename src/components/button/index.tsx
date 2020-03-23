7bf1fc2db968aa1fa32ad5ebd6219a1a21a6eed9
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-03-23 19:48:28 -0300

    feat: add suport to a function on button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { pro } from 'react';
 
 import { Container } from './styles';
+import { GestureResponderEvent } from 'react-native';
 
 type ButtonProps = {
+  onPress?: (event: GestureResponderEvent) => void;
   children: React.ReactElement;
   color?: string;
 };
 
-export default function Button({ children, color }: ButtonProps) {
-  return <Container color={color}>{children}</Container>;
+export default function Button({ onPress, children, color }: ButtonProps) {
+  return (
+    <Container onPress={onPress} color={color}>
+      {children}
+    </Container>
+  );
 }
