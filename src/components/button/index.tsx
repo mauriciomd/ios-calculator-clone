7f176be8082599c8cb0dbe47fceabac3dc604d1c
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-03-23 15:53:35 -0300

    feat: add color support to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,8 +4,9 @@ import { Container } from './styles';
 
 type ButtonProps = {
   children: React.ReactElement;
+  color?: string;
 };
 
-export default function Button({ children }: ButtonProps): React.ReactElement {
-  return <Container>{children}</Container>;
+export default function Button({ children, color }: ButtonProps) {
+  return <Container color={color}>{children}</Container>;
 }
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.TouchableOpacity`
-  background: #ff9999;
+type ButtonPropos = {
+  color?: string;
+};
+
+export const Container = styled.TouchableOpacity<ButtonPropos>`
+  background-color: ${props => (props.color ? props.color : '#444')};
   height: 90px;
   width: 90px;
   border-radius: 45px;
