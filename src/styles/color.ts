3935dddb77190e4ced6698887bff9417c7392b44
Mauricio Matter Donato <mauricio.fear@gmail.com>
2020-03-23 16:48:21 -0300

    refactor: move text color scheme to a separated file

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,4 +1,7 @@
-export const DefaultBackground = '#121212';
+export const DefaultBackgroundColor = '#121212';
 export const DefaultButtonColor = '#414141';
-export const OperationButton = '#fe9c13';
-export const ActionButton = '#b2b2b2';
+export const OperationButtonColor = '#fe9c13';
+export const ActionButtonColor = '#b2b2b2';
+
+export const DefaultTextColor = '#fff';
+export const ActionTextColor = '#000';
